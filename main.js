4929db9de7429b11addf18776a4ed0b3577209bc
Varus80 <60239152+Varus80@users.noreply.github.com>
2020-05-28 21:57:51 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+var m = 0;
+
+
+document.querySelector('.dropdown').addEventListener("click",function(e){
+console.log(m);
+let a = document.querySelector('.dropdown-content');
+let i = 0;
+let b = document.querySelectorAll(".item");
+if(m == 0){
+
+
+a.style.display = "block";
+a.style.flexDirection = "column";
+a.style.alignItems = "center";
+a.style.animation = "menu 1s forwards";
+a.style.justifyContent = "center"
+a.style.width = "100%";
+a.style.height = "100%";
+
+
+for(i;i<b.length;i++){
+b[i].style.color = "red";
+b[i].style.display = "flex";
+b[i].style.justifyContent = "center";
+b[i].style.alignItems = "center";
+b[i].style.width = "98%";
+b[i].style.height = "23.6%";
+b[i].style.border = "5px solid blue";
+}
+}
+else{
+    a.style.display = "none";
+}
+
+if(m == 0){
+m++;
+}
+else{
+    m = 0;
+}
+console.log(m);
+
+
+            
+});
